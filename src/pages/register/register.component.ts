aaeedea9b51a09ef1771692dbd0ff755be6a58bf
anthony vouin <avouin@normandiewebschool.fr>
2024-05-10 16:36:18 +0200

    add register form groupr form builder

--- src/pages/register/register.component.ts
+++ src/pages/register/register.component.ts
@@ -1,42 +1,53 @@
 import { Component } from '@angular/core';
 import { User } from '../../interface/user';
-import { FormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, FormsModule, Validators, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, ReactiveFormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
 export class RegisterComponent {
 
+  constructor(private readonly formBuilder: FormBuilder){}
 
-  userData: User = {
-    firstName: '',
-    lastName: '',
-    email: '',
-    password:'',
-  };
-
+  formGroup: FormGroup = this.formBuilder.group({
+    firstName:["",[Validators.minLength(2), Validators.required]] ,
+    lastName: ["",[Validators.minLength(2), Validators.required]],
+    email: ["",[Validators.email, Validators.required]],
+    password:["",[Validators.minLength(5), Validators.required]],
+  })
+  
   
   registerUser(): void {
-    console.log(this.userData)
-    fetch('http://localhost:3000/auth/register', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(this.userData)
-    })
-    .then(response => response.json())
-    .then(data => {
-      console.log('Réponse du serveur:', data);
+    if(this.formGroup.valid) {
+      const user:User = {
+      firstName:this.formGroup.get("firstName")?.value,
+      lastName:this.formGroup.get("lastName")?.value,
+      email:this.formGroup.get("email")?.value,
+      password:this.formGroup.get("password")?.value
+    }
 
-    })
-    .catch(error => {
-      console.error('Erreur lors de la création du compte:', error);
-    });
+      fetch('http://localhost:3000/auth/register', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(user)
+      })
+      .then(response => response.json())
+      .then(data => {
+        console.log('Réponse du serveur:', data);
+  
+      })
+      .catch(error => {
+        console.error('Erreur lors de la création du compte:', error);
+      });
+
+    }
+   
   }
 
 }
