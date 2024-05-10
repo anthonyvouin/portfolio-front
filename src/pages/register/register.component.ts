9683db8d612bb66168db34c352ba9818032cdc26
anthony vouin <avouin@normandiewebschool.fr>
2024-05-10 20:08:23 +0200

    add jwt

--- src/pages/register/register.component.ts
+++ src/pages/register/register.component.ts
@@ -3,6 +3,8 @@ import { ButtonModule } from 'primeng/button';
 import { Component } from '@angular/core';
 import { User } from '../../interface/user';
 import { FormBuilder, FormGroup, FormsModule, Validators, ReactiveFormsModule } from '@angular/forms';
+import { CookieService } from 'ngx-cookie-service';
+
 
 @Component({
   selector: 'app-register',
@@ -13,7 +15,8 @@ import { FormBuilder, FormGroup, FormsModule, Validators, ReactiveFormsModule } 
 })
 export class RegisterComponent {
 
-  constructor(private readonly formBuilder: FormBuilder){}
+  constructor(private readonly formBuilder: FormBuilder, private readonly cookieService: CookieService) {}
+
 
   formGroup: FormGroup = this.formBuilder.group({
     firstName:["",[Validators.minLength(2), Validators.required]] ,
@@ -24,14 +27,14 @@ export class RegisterComponent {
   
   
   registerUser(): void {
-    if(this.formGroup.valid) {
-      const user:User = {
-      firstName:this.formGroup.get("firstName")?.value,
-      lastName:this.formGroup.get("lastName")?.value,
-      email:this.formGroup.get("email")?.value,
-      password:this.formGroup.get("password")?.value
-    }
-
+    if (this.formGroup.valid) {
+      const user: User = {
+        firstName: this.formGroup.get("firstName")?.value,
+        lastName: this.formGroup.get("lastName")?.value,
+        email: this.formGroup.get("email")?.value,
+        password: this.formGroup.get("password")?.value
+      };
+  
       fetch('http://localhost:3000/auth/register', {
         method: 'POST',
         headers: {
@@ -39,17 +42,23 @@ export class RegisterComponent {
         },
         body: JSON.stringify(user)
       })
-      .then(response => response.json())
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Erreur lors de la création du compte');
+        }
+        return response.json();
+      })
+      
       .then(data => {
         console.log('Réponse du serveur:', data);
-  
+        // Stocker le JWT dans un cookie avec une durée de validité d'une heure
+        document.cookie = `jwt=${data.token}; expires=${new Date(Date.now() + 3600 * 1000).toUTCString()}; path=/`;
+        // Rediriger l'utilisateur vers une autre page par exemple
+        window.location.href = '/accueil';
       })
       .catch(error => {
         console.error('Erreur lors de la création du compte:', error);
       });
-
     }
-   
   }
-
 }
