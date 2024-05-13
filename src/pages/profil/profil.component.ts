02e8264a126100c7253b0b44445eee067ec1cd16
anthony vouin <avouin@normandiewebschool.fr>
2024-05-13 15:38:17 +0200

    add css and update info user

--- src/pages/profil/profil.component.ts
+++ src/pages/profil/profil.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { DeleteAccountComponent } from '../../components/delete-account/delete-account.component';
 import { UpdateUserInfoComponent } from '../../components/update-user-info/update-user-info.component';
+import { UpdatePasswordComponent } from '../../components/update-password/update-password.component';
 
 @Component({
   selector: 'app-profil',
   standalone: true,
-  imports: [DeleteAccountComponent,UpdateUserInfoComponent],
+  imports: [DeleteAccountComponent,UpdateUserInfoComponent, UpdatePasswordComponent],
   templateUrl: './profil.component.html',
   styleUrl: './profil.component.scss'
 })
