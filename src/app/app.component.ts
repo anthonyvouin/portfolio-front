21c3fcca3fa7103c3d10ad189ccc375818bebb77
anthony vouin <avouin@normandiewebschool.fr>
2024-06-17 12:54:30 +0200

    add condition ngif for header footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,15 +2,35 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from '../components/header/header.component';
 import { FooterComponent } from '../components/footer/footer.component';
+import { Router, NavigationEnd } from '@angular/router';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,HeaderComponent, FooterComponent],
+  imports: [RouterOutlet,HeaderComponent, FooterComponent, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
+
 export class AppComponent {
   title:string = 'angularProjetV1';
   number:number = 1
+  showHeaderFooter: boolean = true;
+
+  constructor(private router: Router) {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.checkRoute(event.urlAfterRedirects);
+      }
+    });
+  }
+
+  checkRoute(url: string) {
+    const hideHeaderFooterRoutes = ['/profil'];
+    this.showHeaderFooter = !hideHeaderFooterRoutes.includes(url);
+  }
 }
+
+
+
