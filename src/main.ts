fcc1fdf3e6704299b6b25b0bcd57b1618ec92f5c
anthony vouin <avouin@normandiewebschool.fr>
2024-06-20 23:29:12 +0200

    date formate

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,19 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
 import { JwtHelperService, JWT_OPTIONS } from '@auth0/angular-jwt';
 import { CookieService } from 'ngx-cookie-service';
+import { registerLocaleData } from '@angular/common';
+import * as fr from '@angular/common/locales/fr'
+import { LOCALE_ID } from '@angular/core';
+
+registerLocaleData(fr.default)
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideHttpClient(),
     provideRouter(routes),
     { provide: JWT_OPTIONS, useValue: JWT_OPTIONS },
+    { provide: LOCALE_ID, useValue: 'fr-FR' },
+
     JwtHelperService,
     CookieService
   ]
