46df70cde0d3cf784fb9e4eb04669af101d3e6c0
anthony vouin <avouin@normandiewebschool.fr>
2024-07-16 12:12:54 +0200

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,6 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from '../pages/home/home.component';
 import { LoginComponent } from '../pages/login/login.component';
-import { RegisterComponent } from '../pages/register/register.component';
 import { AuthGuard } from '../guard/auth.guard'
 import { ContactComponent } from '../pages/contact/contact.component';
 import { ProfilComponent } from '../pages/profil/profil.component';
@@ -32,9 +31,7 @@ export const routes: Routes = [
     {
         path:'projets', component: ProjetsComponent,  
     }, 
-    {
-        path:'register', component: RegisterComponent
-    },
+   
 
     {
         path:'login', component: LoginComponent 
